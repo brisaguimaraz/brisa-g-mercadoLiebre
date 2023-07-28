aaa34ccd3bbc6db660f9ecf1d76714afc2fef45b
brisaguimaraz <brisaguimaraz01@gmail.com>
2023-07-28 15:55:39 -0600

    forms listos!

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 /* rutas */
 
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get('/register', (req,res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
 
 
 app.listen(PORT, console.log("Servidor corriendo en http://localhost:" + PORT));
